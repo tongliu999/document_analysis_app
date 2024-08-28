f1e9c4afa59f18dc005cd84e48d2806423d4de55
tongliu999 <tongliu114@gmail.com>
2024-08-28 18:42:19 -0400

    Set up react app to submit files

---
+++ file-upload/src/components/FileForm.js
@@ -0,0 +1,50 @@
+import {useState} from 'react';
+
+function FileForm() {
+    const [file, setFile] = useState(null);
+
+    const handleFileInputChange = (event) => {
+        setFile(event.target.files[0]);
+    }
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        
+        const formData = new FormData();
+        formData.append('file_upload', file);
+
+        try {
+            const endpoint = 'http://localhost:8000/uploadfile/';
+            const response = await fetch(endpoint, {
+                method: 'POST',
+                body: formData
+            });
+
+            if (response.ok) {
+                console.log('File uploaded successfully');
+            } else {
+                console.error('Failed to upload file');
+            }
+        } catch(error) {
+            console.error(error);
+        }
+    }
+
+    return (
+        <div>
+            <h1>Upload file</h1>
+
+            <form onSubmit={handleSubmit}>
+                <div style={{ marginBottom: '20px' }}>
+                    <input type="file" onChange={handleFileInputChange} />
+                </div>
+                <button type="submit">Upload</button>
+
+            </form>
+
+            { file && <p>{file.name}</p>}
+        </div>
+    )
+}
+
+export default FileForm;
