b0697f17fc1c6ddd7a5a70876e49e582957ca93f
tongliu999 <tongliu114@gmail.com>
2024-09-12 07:58:32 -0400

    updated backend to get rid of need for file permissions

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-from fastapi import FastAPI, UploadFile, Form, File
+from fastapi import FastAPI, UploadFile, Form
 from fastapi.middleware.cors import CORSMiddleware
-from pathlib import Path
 import pytesseract
 from PIL import Image
 import os
 import sys
 from reader import *
-
-UPLOAD_DIR = Path() / 'uploads'
+import io
 
 app = FastAPI()
 app.add_middleware(
@@ -21,10 +19,9 @@ app.add_middleware(
 @app.post('/uploadfile/')
 async def create_upload_file(file_upload: UploadFile, summarizer_value: int = Form(...)):
     data = await file_upload.read()
-    save_to = UPLOAD_DIR / file_upload.filename
-    with open(save_to, 'wb') as f:
-        f.write(data)    
+    
+    # Process the file data directly
+    image = Image.open(io.BytesIO(data))
+    text = pytesseract.image_to_string(image)
 
-    print(f"Summarizer Value: {summarizer_value}")
-
-    return scan(summarizer_value)
+    return scan(text, summarizer_value)
--- reader.py
+++ reader.py
@@ -5,25 +5,12 @@ import os
 import sys
 from summary import summarize
 
-def scan(per): 
-    path ="C:\\document_analysis_app\\uploads"
+def scan(text, per): 
+    #path ="C:\\document_analysis_app\\uploads"
+    #tempPath ="C:\\document_analysis_app\\newFiles"           
+    #inputPath = os.path.join(path, imageName) 
+    #img = Image.open(inputPath) 
 
-    tempPath ="C:\\document_analysis_app\\newFiles"
-
-    for imageName in os.listdir(path): 
-              
-        inputPath = os.path.join(path, imageName) 
-        img = Image.open(inputPath) 
-  
-        text = pytesseract.image_to_string(img, lang ="eng") 
-  
-        imageName = imageName.split('.')[0]
-  
-        fullTempPath = os.path.join(tempPath, 'time_'+imageName+".txt") 
-        
-        text = summarize(text, per/100) 
-        text = text.replace('\n', ' ')
-
-        os.remove(inputPath)  
-    
-    return {text, per}
+    text = summarize(text, per/100) 
+    text = text.replace('\n', ' ')
+    return text
